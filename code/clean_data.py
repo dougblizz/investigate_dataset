932435bd0cb64540e141f2cac9ba84f2cefd3b70
docando <docando@clmconsultores.com>
2020-04-17 14:06:10 -0400

    update readme and add some features

--- code/clean_data.py
+++ code/clean_data.py
@@ -4,11 +4,9 @@
 @author: dougblizz
 """
 
-import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt
-from pandas.plotting import scatter_matrix
-import seaborn as sns;
+import seaborn as sns
 import funct
 
 plt.style.use('ggplot')
@@ -16,121 +14,157 @@ plt.style.use('ggplot')
 
 df = pd.read_csv('../dataset/tmdb-movies.csv')
 
-#Create df_ movies
+# Create df_ movies
 df_movies = df[['popularity', 'budget', 'revenue', 'original_title', 'cast', 'director', 'runtime', 'genres', 
                 'production_companies', 'release_date','vote_count', 'vote_average', 'release_year', 'budget_adj', 'revenue_adj']]
 
-#Remove unnecessary rows
+# Remove unnecessary rows
 df_movies = df_movies[(df_movies['budget'] != 0) & (df_movies['revenue'] != 0)]
 
-#Drop na values
+# Drop na values
 df_movies.dropna(inplace = True)
 
+# Dataframe without outliers
+Q1 = df_movies.quantile(0.25)
+Q3 = df_movies.quantile(0.75)
+IQR = Q3 - Q1
+df_without_outliers = df_movies[((df_movies < (Q1 - 1.5 * IQR)) | (df_movies > (Q3 + 1.5 * IQR))).any(axis=1)]
+
 # Dataframe with genres
-df_genres = funct.clean_columns_with_pipeline(df_movies, 'genres')
+df_genres = funct.clean_columns_with_pipeline(df_without_outliers, 'genres')
 
 # Dataframe with cast
-df_cast = funct.clean_columns_with_pipeline(df_movies, 'cast')
+df_cast = funct.clean_columns_with_pipeline(df_without_outliers, 'cast')
 
-#Dataframe with production_companies
-df_production = funct.clean_columns_with_pipeline(df_movies, 'production_companies')
+# Dataframe with production_companies
+df_production = funct.clean_columns_with_pipeline(df_without_outliers, 'production_companies')
     
-#Dataframe with director
-df_director = funct.clean_columns_with_pipeline(df_movies, 'director')
+# Dataframe with director
+df_director = funct.clean_columns_with_pipeline(df_without_outliers, 'director')
 
-# Directores con mas peliculas con matplotlib
-# plt.subplots(figsize = (16,12))
-# df_director.director.value_counts().head().plot(kind = 'bar', label = 'movies per director')\
-#                     .set_title('The 5 directors with the most films produced', fontsize = 20)
-# plt.xlabel('directors', fontsize = 16)
-# plt.ylabel('quantity', fontsize = 16)
-# plt.legend(loc='upper right', fontsize = 16)
-# plt.xticks(rotation = 360)
 
-#Number of films per year.
+# Number of films per year.
 plt.subplots(figsize = (16,12))
-fig = sns.kdeplot(df_movies['release_year'], color = 'g', shade=True, label = 'movies')
-plt.axvline(df_movies['release_year'].max(), linestyle='--', color='k', label='Threshold')
-plt.legend(title=None, loc='upper right')
+fig = sns.kdeplot(df_movies['release_year'],\
+                  color = 'g',\
+                  shade = True,\
+                  label = 'movies')\
+                  .set_title('Number of films per year',\
+                       fontsize = 20)
 
-# Directores con mas peliculas con seaborn
-plt.subplots(figsize = (16,12))
-yo = df_director.director.value_counts().head()
-yo = yo.to_frame()
-yo.reset_index(level=0, inplace=True)
-yo.rename(columns={'director': 'quantity'}, inplace=True)
-yo.rename(columns={'index': 'director'}, inplace=True)
-sns.barplot(x="director", y="quantity", data=yo).set_title('The 5 directors with the most films produced', fontsize = 20)
+plt.axvline(df_movies['release_year'].max(), linestyle = '--', color = 'k', label = 'Threshold')
+plt.legend(loc='upper right')
 
 
-# Directores populares
-au = df_director.director.value_counts() >= 10
-au = au.to_frame() 
-au.reset_index(level=0, inplace=True)
-au.rename(columns={'index': 'dir'}, inplace=True)
-au = au.dir[au['director'] == True]
-
-
-real = df_director[df_director['director'].isin(au)]
-
-x = real.groupby('director')['popularity'].max()
-x.sort_values(ascending = False, inplace = True)
-x = x.to_frame()
-x.reset_index(level=0, inplace=True)
-
-plt.subplots(figsize = (16,12))
-sns.barplot(x="director", y="popularity", data=x.head(),  palette="Blues_d")
-
-
-#Which genres are most popular from year to year? (Probando)
+# Which genres are most popular from years
 frames = []
 cont = 1960
 while(True):
     if cont <= 2010:
-        frames.append(df_genres[df_genres['release_year'] == cont].groupby('genres')[['release_year','popularity']].max().head())
+        aux = df_genres[df_genres['release_year'] == cont].groupby('genres')['popularity'].mean()
+        aux = aux.to_frame()
+        aux.reset_index(level=0, inplace=True)
+        aux['year'] = cont
+        aux.sort_values(by = 'popularity', ascending = False)
+        frames.append(aux.head())
     else:
-        frames.append(df_genres[df_genres['release_year'] == 2015].groupby('genres')[['release_year','popularity']].max().head())
+        aux = df_genres[df_genres['release_year'] == 2015].groupby('genres')['popularity'].mean()
+        aux = aux.to_frame()
+        aux.reset_index(level=0, inplace=True)
+        aux['year'] = 2015
+        aux.sort_values(by = 'popularity', ascending = False)
+        frames.append(aux.head())
         break
     cont += 10
 
-result = pd.concat(frames)
-result.reset_index(level=0, inplace=True)
+new_genres = pd.concat(frames)
+new_genres.reset_index(level=0, inplace=True)
 
-plt.subplots(figsize = (20,18))
-sns.barplot(x = "release_year", y = "popularity", hue = 'genres', data=result).set_title(f'Most popular genres', fontsize = 20)
+plt.subplots(figsize = (16,12))
+sns.barplot(x = "year",
+            y = "popularity",\
+            hue = 'genres',\
+            data = new_genres)\
+            .set_title('Most popular genres', fontsize = 20)
 
-#What kinds of propertiesare associated with movies that have high revenues?
-#Headmap
+
+    
+# What kinds of properties are associated with movies that have high revenues?
+
+# Headmap with correlation
 f,ax = plt.subplots(figsize = (18,14))
-sns.heatmap(df_movies.corr(), annot = True, linewidths=.5, fmt = '.1f', ax = ax)
+sns.heatmap(df_movies.corr(),\
+            annot = True,\
+            linewidths=.5,\
+            fmt = '.1f',\
+            ax = ax)
+
 
 # Revenue vs Budget
-plt.subplots(figsize = (14,12))
-splot = sns.scatterplot(x='revenue', y='budget', hue ='release_year', size = 'release_year', alpha = 0.5, sizes = (20,300), data=df_movies)
-# splot.set(xscale="log")
-# splot.set(yscale="log")
+funct.plot_scatter(df_movies, 'revenue', 'budget', 'release_year')
 
 # Revenue vs vote_count
-plt.subplots(figsize = (14,12))
-sns.scatterplot(x='revenue', y='vote_count', hue ='release_year', size = 'release_year', alpha = 0.5, sizes = (20,300), data=df_movies)
+funct.plot_scatter(df_movies, 'revenue', 'vote_count', 'release_year')
+    
+# Revenue vs popularity
+funct.plot_scatter(df_movies, 'revenue', 'popularity', 'release_year')
 
-# Revenue vs vote_count
-plt.subplots(figsize = (14,12))
-sns.scatterplot(x='revenue', y='popularity', hue ='release_year', size = 'release_year', alpha = 0.5, sizes = (20,300), data=df_movies)
+# Director min value (Subjective)
+df_director.director.value_counts().head(20)
 
+# Directors with more movies
+funct.quantity(df_director, 'director')
 
+# Most popular directors
+funct.popular(df_director, 'director', 9)
+    
+# Most revenue directors
+funct.revenue(df_director, 'director', 9)
 
+# Directors with high budget
+funct.budget(df_director, 'director', 9)
 
-# sns.regplot(x = 'revenue', y = 'popularity', ci = 100, data = df_movies)
+# Actors min value (Subjective)
+df_cast.cast.value_counts().head(20)
 
-# boxplot
-# plt.subplots(figsize = (20,16))
-# sns.boxplot(x = 'genres', y = 'popularity', data = df_genres[df_genres['release_year'] == 2010])
+# Actors with more movies
+funct.quantity(df_cast, 'cast')
 
-# f,ax = plt.subplots(figsize = (18,14))
-# sns.heatmap(df_aux.corr(), annot = True, linewidths=.5, fmt = '.1f', ax = ax)
-# plt.show()
+# Most popular actors
+funct.popular(df_cast, 'cast', 10)
 
-# df_aux[df_aux['release_year'] == 2015].groupby('genres')['release_year'].count().plot(kind = 'bar')
+# Most revenue actors
+funct.revenue(df_cast, 'cast', 10)
 
-# scatter_matrix(values, alpha=0.2, figsize=(18, 18))
+# Actors with high budget
+funct.budget(df_cast, 'cast', 10)
+
+# production_companies min value (Subjective)
+df_production.production_companies.value_counts().head(20)
+
+# production_companies with more movies
+funct.quantity(df_production, 'production_companies')
+
+# Most popular production_companies
+funct.popular(df_production, 'production_companies', 10)
+
+# Most revenue production_companies
+funct.revenue(df_production, 'production_companies', 10)
+
+# Most revenue genres
+funct.budget(df_production, 'production_companies', 10)
+
+# genres min value (Subjective)
+df_genres.genres.value_counts().min()
+
+# genres with more movies
+funct.quantity(df_genres, 'genres')
+
+# Most popular genres
+funct.popular(df_genres, 'genres', df_genres.genres.value_counts().min())
+
+# Most revenue genres
+funct.revenue(df_genres, 'genres', df_genres.genres.value_counts().min())
+
+# Most budget genres
+funct.budget(df_genres, 'genres', df_genres.genres.value_counts().min())
